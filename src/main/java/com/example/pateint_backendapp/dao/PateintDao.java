b009978f88ffac0507100a4bc9f2953360acc783
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-08 12:58:51 +0530

    delete

--- src/main/java/com/example/pateint_backendapp/dao/PateintDao.java
+++ src/main/java/com/example/pateint_backendapp/dao/PateintDao.java
@@ -1,6 +1,8 @@
 package com.example.pateint_backendapp.dao;
 
 import com.example.pateint_backendapp.model.Pateint;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +13,11 @@ public interface PateintDao extends CrudRepository<Pateint,Integer> {
 
     @Query(value = "SELECT `id`, `address`, `age`, `mobno`, `name` FROM `pateints` WHERE `name`=:name",nativeQuery = true)
     List<Pateint> searchPateint(@Param("name") String name);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `pateints` WHERE `id`=:id",nativeQuery = true)
+    void deletePateint(@Param("id") Integer id);
 
 }
